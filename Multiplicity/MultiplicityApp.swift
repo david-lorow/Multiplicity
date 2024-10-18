5f45c4610c5389cd770a1aa7e42af5a13944cd5e
David <david.lorow@gmail.com>
2024-10-18 02:57:27 -0400

    Initial Commit

---
+++ Multiplicity/MultiplicityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicityApp.swift
+//  Multiplicity
+//
+//  Created by David Lorow on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
